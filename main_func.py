1109dfe616bfbee5b913f2411684b7a91042a488
Andrew Donoho <github@store.ddg.com>
2021-05-04 16:05:46 -0500

    Expand to try to use remote clusters.

--- main_func.py
+++ main_func.py
@@ -1,41 +1,39 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-@author: mahsa lotfi
+@author: Andrew W. Donoho based on work by Mahsa Lotfi
 Main module for phase transition
 """
 
-from dask import delayed, compute, persist, visualize
-from dask.distributed import Client, LocalCluster, wait, as_completed
-from problem.problemSpec import problemInit
+from dask import delayed
+from dask.distributed import Client, wait, as_completed, LocalCluster
+from dask_kubernetes import HelmCluster
+from dask_cloudprovider.gcp import GCPCluster
 from problem.loop import inner_loop
+from problem.problemSpec import problemInit
 
-gClient = None
 # gRange = 99
 gRange = 12
 
 
-def phase_transition():
+def phase_transition(client: Client):
     p = problemInit()
     m = p.m
     loop = delayed(inner_loop)
 
-    # results = []
-    # for i in range(gRange):
-    #     result = loop(int(m[i]))
-    #     results.append(result)
     loops = [loop(int(m[i])) for i in range(gRange)]
     # visualize(loops, filename='delayed_results', format='svg')
-    futures = gClient.compute(loops)
+    futures = client.compute(loops)
     wait(futures)
-    # for future in futures:
-    #     print(future.result)
     for future, result in as_completed(futures, with_results=True):
         print(result)
-    # print(results)
 
 
 if __name__ == "__main__":
-    cluster = LocalCluster(dashboard_address='localhost:8787')
-    gClient = Client(cluster)
-    phase_transition()
+    # with LocalCluster(dashboard_address='localhost:8787') as cluster:
+    # with GCPCluster(projectid='superb-garden-303018', machine_type='n1-standard-4', n_workers=2) as cluster:
+    #     with Client(cluster) as client:
+    #         phase_transition(client)
+    cluster = HelmCluster(release_name='gke-dask')
+    with Client(cluster) as client:
+        phase_transition(client)
